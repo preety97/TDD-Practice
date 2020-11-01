95827a5adee7eec8e15fb7936a79de70f14df32d
vivekbhakhri <43738215+vivekbhakhri@users.noreply.github.com>
2020-11-01 11:40:47 -0500

    Cleaned up code

--- test/unitTests.js
+++ test/unitTests.js
@@ -6,15 +6,19 @@ describe('sum', function(){
     it('should return 6 whn adding 3 and 3', function(){
         assert.equal(functions.sum(3, 3), 6);
     });
+    
     it('should return -6 whn adding -3 and -3', function(){
         assert.equal(functions.sum(-3, -3), -6);
     });
+    
     it('should return 3 whn adding 6 and -3', function(){
         assert.equal(functions.sum(6, -3), 3);
     });
+    
     it('should return 0 whn adding 0 and 0', function(){
         assert.equal(functions.sum(0, 0), 0);
     });
+    
     it('should return 9.8 whn adding 6 and 3.8', function(){
         assert.equal(functions.sum(6, 3.8), 9.8);
     });
@@ -23,30 +27,36 @@ describe('product', function(){
     it('should return 6 whn doing product of 2 and 3', function(){
         assert.equal(functions.product(2, 3), 6);
     });
+    
     it('should return 0 whn doing product of 0 and 4', function(){
         assert.equal(functions.product(0, 4), 0);
     });
+    
     it('should return 1 whn doing product of 1 and 1', function(){
         assert.equal(functions.product(1, 1), 1);
     });
+    
     it('should return -2 whn doing product of  -1 and 2', function(){
         assert.equal(functions.product(-1, 2), -2);
-    });
-   
+    });  
 });
 describe('min', function(){
     it('should return 1 whn comparing 1 and 2', function(){
         assert.equal(functions.min(1, 2), 1);
     });
+    
     it('should return -4 whn comparing -4 and -1', function(){
         assert.equal(functions.min(-4, -1), -4);
     });
+    
     it('should return -5 whn comparing 5 and -5', function(){
         assert.equal(functions.min(5, -5), -5);
     });
+    
     it('should return 0 whn comparing 5 and 0', function(){
         assert.equal(functions.min(5, 0), 0);
     });
+    
     it('should return 5 whn comparing 5 and 5', function(){
         assert.equal(functions.min(5, 5), 5);
     });
@@ -55,15 +65,19 @@ describe('max', function(){
     it('should return 2 whn comparing 1 and 2', function(){
         assert.equal(functions.max(1, 2), 2);
     });
+    
     it('should return -1 whn comparing -4 and -1', function(){
         assert.equal(functions.max(-4, -1), -1);
     });
+    
     it('should return 5 whn comparing 5 and -5', function(){
         assert.equal(functions.max(5, -5), 5);
     });
+    
     it('should return 4 whn comparing 4 and 0', function(){
         assert.equal(functions.max(4, 0), 4);
     });
+    
     it('should return 5 whn comparing 5 and 5', function(){
         assert.equal(functions.max(5, 5), 5);
     });
@@ -72,45 +86,55 @@ describe('head', function(){
     it('should return null if array is empty', function(){
         assert.equal(functions.head([]), null);
     });
+    
    it('should return "Gwen" if array is ["Gwen","Ben","Max"]', function(){
         assert.equal(functions.head(["Gwen","Ben","Max"]), "Gwen");
     });
+    
     it('should return 1 if array is [1,2,3]', function(){
         assert.equal(functions.head([1,2,3]), 1);
     });
+    
     it('should return 2 if array is [2]', function(){
         assert.equal(functions.head([2]), 2);
     });
-   
 });
+
 describe('tail', function(){
     it('should return null if array is empty', function(){
         assert.equal(functions.tail([]), null);
     });
+    
    it('should return "Max" if array is ["Gwen","Ben","Max"]', function(){
         assert.equal(functions.tail(["Gwen","Ben","Max"]), "Max");
     });
+    
     it('should return 3 if array is [1,2,3]', function(){
         assert.equal(functions.tail([1,2,3]), 3);
     });
+    
     it('should return 2 if array is [2]', function(){
         assert.equal(functions.tail([2]), 2);
     });
-   
 });
+
 describe('mid', function(){
     it('should return null if array is empty', function(){
         assert.equal(functions.mid([]), null);
     });
+    
     it('should return "Ben" if array is ["Gwen","Ben","Max"]', function(){
         assert.equal(functions.mid(["Gwen","Ben","Max"]), "Ben");
     });
+    
     it('should return 2 if array is [1,2,3]', function(){
         assert.equal(functions.mid([1,2,3]), 2);
     });
+    
     it('should return 2 if array is [2]', function(){
         assert.equal(functions.mid([2]), 2);
     });
+    
     it('should return [2,3] if array is [1,2,3,4]', function(){
         assert.deepEqual(functions.mid([1,2,3,4]),[2,3]);
     });
@@ -120,15 +144,19 @@ describe('total', function(){
     it('should return 0 if array is empty', function(){
         assert.equal(functions.total([]), 0);
     });
+    
     it('should return 4 if array is [1,2,1]', function(){
         assert.equal(functions.total([1,2,1]), 4);
     });
+    
     it('should return 6 if array is [-2,5,3]', function(){
         assert.equal(functions.total([-2,5,3]), 6);
     });
+    
     it('should return -6 if array is [-1,-3,-2]', function(){
         assert.equal(functions.total([-1,-3,-2]), -6);
     });
+    
     it('should return 1 if array is [1]', function(){
         assert.equal(functions.total([1]), 1 );
     });
@@ -138,18 +166,23 @@ describe('smallest', function(){
     it('should return null if array is empty', function(){
         assert.equal(functions.smallest([]), null);
     });
+    
     it('should return 1 if array is [10,5,1]', function(){
         assert.equal(functions.smallest([10,5,1]), 1);
     });
+    
     it('should return 1 if array is [1,2,1]', function(){
         assert.equal(functions.smallest([1,2,1]), 1);
     });
+    
     it('should return -2 if array is [-2,5,3]', function(){
         assert.equal(functions.smallest([-2,5,3]), -2);
     });
+    
     it('should return -3 if array is [-1,-3,-2]', function(){
         assert.equal(functions.smallest([-1,-3,-2]), -3 );
     });
+    
     it('should return 1 if array is [1]', function(){
         assert.equal(functions.smallest([1]), 1 );
     });
@@ -159,18 +192,23 @@ describe('largest', function(){
     it('should return null if array is empty', function(){
         assert.equal(functions.largest([]), null);
     });
+    
     it('should return 10 if array is [10,5,1]', function(){
         assert.equal(functions.largest([10,5,1]), 10);
     });
+    
     it('should return 2 if array is [1,2,1]', function(){
         assert.equal(functions.largest([1,2,1]), 2);
     });
+    
     it('should return 5 if array is [-2,5,3]', function(){
         assert.equal(functions.largest([-2,5,3]), 5);
     });
+    
     it('should return -1 if array is [-1,-3,-2]', function(){
         assert.equal(functions.largest([-1,-3,-2]), -1 );
     });
+    
     it('should return 1 if array is [1]', function(){
         assert.equal(functions.largest([1]), 1 );
     });
@@ -182,6 +220,7 @@ describe('merge', function(){
         let arr2 = [];
         assert.isNull(functions.merge(arr1,arr2));
     });
+    
     it('should return [4,-9,7,0] if array is [4,7] and [-9,0]', function(){
         let arr1 = [4,7];
         let arr2 = [-9,0];
